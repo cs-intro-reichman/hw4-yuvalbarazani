ee28dd3e470796fa7ec65ad9f7c44e10503ef5d3
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-12-02 19:26:42 +0200

    for testing

--- Primes.java
+++ Primes.java
@@ -1,5 +1,31 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        final int N = Integer.parseInt(args[0]);
+        int p = 2;
+        boolean[] arr=new boolean[N];
+        arr[0]= true;
+        arr[1]=true;
+         
+        while(p <= Math.sqrt(N)){
+            
+            for(int i = 0;i<N;i++){
+                if(i % p == 0 && i != p)
+                    arr[i]=true;
+            }
+            p++;
+
+            while(p<Math.sqrt(N) && arr[p])
+            {
+                p++;
+            }
+        }
+
+        System.out.println("Prime numbers up to " + N + ":");
+        for(int i=0;i<N;i++){
+
+            if(!arr[i]){
+                System.out.println(i);
+            }          
+        }
     }
 }
