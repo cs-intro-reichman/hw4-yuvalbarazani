70d181f3a1321f14f51d1cb1334bc75c5a5a3695
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-12-03 01:35:04 +0200

    for testing

--- MyString.java
+++ MyString.java
@@ -18,13 +18,36 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        for(int i= 0; i<str.length();i++){
+            if(str.charAt(i)>'A'&& str.charAt(i)<'Z'){
+                str=str.substring(0,i) + (str.charAt(i)+32) + str.substring(i+1, str.length());
+            }
+        }
+        return str;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         // Replace the following statement with your code
+        if(str2.length()>str1.length())
+            return false;
+        int j;
+       
+        for(int i= 0;i<str1.length();i++){
+
+            if(str1.charAt(i)==str2.charAt(0)){
+
+                j=0;
+                for(int k=i ;k<str1.length() && j<str2.length(); k++){
+                    if(str1.charAt(k) != str2.charAt(j))
+                        break;
+                    j++;
+                }
+                if(j==str2.length())
+                    return true;
+            }
+
+        }
         return false;
     }
 }
